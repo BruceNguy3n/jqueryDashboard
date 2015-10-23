36ea58b4d000ed7495caaf88b49697ca1334e720
Bruce Nguyen <khoanguyendnvn@gmail.com>
2015-10-23 09:32:22 +0700

    Project structure

--- js/dashboard.js
+++ js/dashboard.js
@@ -42,7 +42,37 @@ var dashboard =
 	},
 	initSharing: function()
 	{
+		$('.shareBox a').on('click', function()
+		{
+			var type = $(this).prop('type');
+			dashboard.sharePage(type);
+		});
+	},
+	sharePage: function(shareType)
+	{
+		var pageUrl = encodeURIComponent(document.location);
+		var shareUrl;
 
+		switch(shareType)
+		{
+			case 'fb':
+				shareUrl = 'https://www.facebook.com/share/sharer.php?u=' + pageUrl;
+				break;
+			case 'tweet':
+				shareUrl = 'https://twitter.com/intent/tweet?text=Check out my page&url=' + pageUrl
+					+ '&via=v08i';
+				break;
+			case 'reddit':
+				shareUrl = 'http://reddit.com/submit?url=' + pageUrl;
+				break;
+			case 'gplus':
+				shareUrl = 'https://plus.google.com/share?url=' + pageUrl;
+				break;
+			default:
+				return false;
+		}
+
+		window.open(shareUrl, '', 'width=600,height:500');
 	},
 	initFlickr: function()
 	{
